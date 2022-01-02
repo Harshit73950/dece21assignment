b3625012b93237595635f512ddd4c5c44956f366
Harshit73950 <94857400+Harshit73950@users.noreply.github.com>
2022-01-02 13:28:54 +0530

    Add files via upload

---
+++ decm232.js
@@ -0,0 +1,9 @@
+et n = prompt("Enter a number");
+//prompt gives string using parsenInt convart number datatype .
+n = parseInt(n);
+
+// using for loop why ==> 2 to n get 
+for (let i = 2; i <= n; i = i + 2) {
+    //in loop increment +2 why ==> +2 get  even number
+    console.log(i);
+    // print 2 to n using console i
