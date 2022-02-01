92e4bbc881b3536c9fbeda8f094cf6725cfeadb2
Harshit73950 <94857400+Harshit73950@users.noreply.github.com>
2022-02-01 00:57:40 -0800

    Add files via upload

--- qs1.js
+++ qs1.js
@@ -1,13 +1,10 @@
-var a=0;
-b=document.getElementById("counting").innerText;
-b.innerText= a;
-
-function increment() {
-    a=a+1;
-    document.getElementById("counting").innerText= a;
-}
-
-function decrement() {
-    a=a-1;
-    document.getElementById("counting").innerText= a;
+var arr = [2,7,11,15,14,5,9,6];
+var target = 19;
+for(let i = 0 ; i < arr.length ; i++){
+    for (let j = i + 1; j <= arr.length - 1; j++){
+        if(target == arr[i] + arr[j]){
+            console.log([i,j]);
+            document.write(`The indexes of values total = 19 is &nbsp;${[i,j]}`)
+        }
+    }
 }
